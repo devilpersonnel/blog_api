b92ea30e348a5c157eb8e2777ca4df5bb4be0e2a
devilpersonnel <devilpersonnel@gmail.com>
2016-04-27 02:03:07 +0545

    Creates, updates, deletes the article

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
   namespace :api, :defaults => {format: 'json'} do
     namespace :v1 do
-
+      resources :articles, :only => [:show, :create, :update, :destroy] do
+        resources :comments
+      end
     end
   end
 
